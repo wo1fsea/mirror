2405f10acd9fadf6bb94b2fb11205b53013a8781
wo1fsea <wo1fsea@126.com>
2020-03-30 21:14:13 +0800

    mod: add function_traits

---
+++ mirror/headers/function_traits.hpp
@@ -0,0 +1,186 @@
+#pragma once
+#ifndef _MIRROR_FUNCTION_TRAITS_
+#define _MIRROR_FUNCTION_TRAITS_
+
+#include <type_traits>
+#include <tuple>
+
+namespace mirror 
+{
+	template <typename raw_signature>
+	class function_traits 
+	{
+		using return_type = void;
+		
+		using void_signature = void*;
+		using raw_signature = void*;
+		using signature = void*;
+
+		using args_tuple = std::tuple<>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = false;
+
+		static constexpr size_t args_size = 0;
+		static constexpr bool has_no_return = false;
+	};
+	
+	// function
+	template <typename return_type, typename... args_types>
+	struct function_traits<return_type(args_types...)>
+	{
+		using void_signature = void*;
+		using raw_signature = return_type(args_types...);
+		using signature = return_type(*)(args_types...);
+
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = true;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = false;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename return_type, typename... args_types>
+	struct function_traits<return_type(args_types...) noexcept>
+	{
+		using void_signature = void*;
+		using raw_signature = return_type(args_types...) noexcept;
+		using signature = return_type(*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = true;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = false;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename return_type, typename... args_types>
+	struct function_traits<return_type(*)(args_types...)>
+	{
+		using void_signature = void*;
+		using raw_signature = return_type(*)(args_types...);
+		using signature = return_type(*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = true;
+		static constexpr bool is_mem_func_ptr = false;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename return_type, typename... args_types>
+	struct function_traits<return_type(*)(args_types...) noexcept>
+	{
+		using void_signature = void*;
+		using raw_signature = return_type(*)(args_types...) noexcept;
+		using signature = return_type(*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = true;
+		static constexpr bool is_mem_func_ptr = false;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename instance_type, typename return_type, typename... args_types>
+	struct function_traits<return_type(instance_type::*)(args_types...)>
+	{
+		using void_signature = void(instance_type::*)(void);
+		using raw_signature = return_type(instance_type::*)(args_types...);
+		using signature = return_type(instance_type::*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = true;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename instance_type, typename return_type, typename... args_types>
+	struct function_traits<return_type(instance_type::*)(args_types...) noexcept>
+	{
+		using void_signature = void(instance_type::*)(void);
+		using raw_signature = return_type(instance_type::*)(args_types...)noexcept;
+		using signature = return_type(instance_type::*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = true;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename instance_type, typename return_type, typename... args_types>
+	struct function_traits<return_type(instance_type::*)(args_types...) const>
+	{
+		using void_signature = void(instance_type::*)(void);
+		using raw_signature = return_type(instance_type::*)(args_types...) const;
+		using signature = return_type(instance_type::*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = true;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+	template <typename instance_type, typename return_type, typename... args_types>
+	struct function_traits<return_type(instance_type::*)(args_types...) const noexcept>
+	{
+		using void_signature = void(instance_type::*)(void);
+		using raw_signature = return_type(instance_type::*)(args_types...) const noexcept;
+		using signature = return_type(instance_type::*)(args_types...);
+		
+		using args_tuple = std::tuple<args_types...>;
+		template <int i>
+		using i_args_type = typename std::tuple_element<i, args_tuple>::type;
+
+		static constexpr bool is_func = false;
+		static constexpr bool is_func_ptr = false;
+		static constexpr bool is_mem_func_ptr = true;
+
+		static constexpr size_t args_size = std::tuple_size_v<args_tuple>;
+		static constexpr bool has_no_return = std::is_void<return_type>::value;
+	};
+
+} // namespace mirror
+
+#endif
